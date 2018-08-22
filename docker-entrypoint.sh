70b37ac11505c8cec2124bf9340e68c5e3681f0f
Bryan Larson <b32147@gmail.com>
2018-08-22 14:01:02 -0400

    Configured environment variables to default to AWS PS if not defined

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,21 +2,21 @@
 set -e
 
 # Retrieve database details
-export KONG_DATABASE=postgres
-export KONG_PG_HOST=$(aws ssm get-parameters --names $PS_PATH.db_host --with-decryption --region us-east-1 | jq -r '.Parameters[].Value')
-export KONG_PG_USER=$(aws ssm get-parameters --names $PS_PATH.db_user --with-decryption --region us-east-1 | jq -r '.Parameters[].Value')
-export KONG_PG_PASSWORD=$(aws ssm get-parameters --names $PS_PATH.db_password --with-decryption --region us-east-1 | jq -r '.Parameters[].Value')
-export KONG_PG_DATABASE=$(aws ssm get-parameters --names $PS_PATH.db_database --with-decryption --region us-east-1 | jq -r '.Parameters[].Value')
+export KONG_DATABASE=${KONG_DATABASE:=postgres}
+export KONG_PG_HOST=${KONG_PG_HOST:=$(aws ssm get-parameters --names $PS_PATH.db_host --with-decryption --region us-east-1 | jq -r '.Parameters[].Value')}
+export KONG_PG_USER=${KONG_PG_USER:=$(aws ssm get-parameters --names $PS_PATH.db_user --with-decryption --region us-east-1 | jq -r '.Parameters[].Value')}
+export KONG_PG_PASSWORD=${KONG_PG_PASSWORD:=$(aws ssm get-parameters --names $PS_PATH.db_password --with-decryption --region us-east-1 | jq -r '.Parameters[].Value')}
+export KONG_PG_DATABASE=${KONG_PG_DATABASE:=$(aws ssm get-parameters --names $PS_PATH.db_database --with-decryption --region us-east-1 | jq -r '.Parameters[].Value')}
 
 # Configure other parts of the API
-export KONG_PROXY_LISTEN=$(aws ssm get-parameters --names $PS_PATH.proxy_listen --with-decryption --region us-east-1 | jq -r '.Parameters[].Value')
-export KONG_ADMIN_LISTEN=$(aws ssm get-parameters --names $PS_PATH.admin_listen --with-decryption --region us-east-1 | jq -r '.Parameters[].Value')
+export KONG_PROXY_LISTEN=${KONG_PROXY_LISTEN:=$(aws ssm get-parameters --names $PS_PATH.proxy_listen --with-decryption --region us-east-1 | jq -r '.Parameters[].Value')}
+export KONG_ADMIN_LISTEN=${KONG_ADMIN_LISTEN:=$(aws ssm get-parameters --names $PS_PATH.admin_listen --with-decryption --region us-east-1 | jq -r '.Parameters[].Value')}
 
 # Setup logging
-export KONG_PROXY_ACCESS_LOG=/dev/stdout
-export KONG_ADMIN_ACCESS_LOG=/dev/stdout
-export KONG_PROXY_ERROR_LOG=/dev/stderr
-export KONG_ADMIN_ERROR_LOG=/dev/stderr
+export KONG_PROXY_ACCESS_LOG=${KONG_PROXY_ACCESS_LOG:=/dev/stdout}
+export KONG_ADMIN_ACCESS_LOG=${KONG_ADMIN_ACCESS_LOG:=/dev/stdout}
+export KONG_PROXY_ERROR_LOG=${KONG_PROXY_ERROR_LOG:=/dev/stderr}
+export KONG_ADMIN_ERROR_LOG=${KONG_ADMIN_ERROR_LOG:=/dev/stderr}
 
 # Waiting for postgres
 until psql --host=$KONG_PG_HOST --username=$KONG_PG_USER $KONG_PG_DATABASE -w &>/dev/null
